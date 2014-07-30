04a2cc115e8e91d3bfe6d719a3e2e3fe1e6085af
nagraj1981 <nagrajsundram1981@gmail.com>
2014-07-30 15:10:17 +0530

    df

--- IT Asset Management Project/Program.cs
+++ IT Asset Management Project/Program.cs
@@ -16,18 +16,19 @@ namespace IT_Asset_Management_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AdminForm());
+           // Application.Run(new AdminForm());
             LoginPage objLog = new LoginPage();
-            // objLog.ShowDialog();
+            objLog.ShowDialog();
 
             if(objLog.IsAuthenticated)
             {
-                switch(objLog.RoleID)
+                PCVASSoftwareSelectionMenu objMenu = new PCVASSoftwareSelectionMenu();
+                objMenu.RoleID = int.Parse(objLog.RoleID.ToString());
+                objMenu.ShowDialog();
+             /*   switch(objLog.RoleID)
                 {    case 1:
-   
-                    AdminForm objAdmin = new AdminForm();
-                        objAdmin.UserName = objLog.UserName.ToString();
-                        objAdmin.ShowDialog();
+                       
+                   
                         
                     break;
                     case 2:
@@ -38,7 +39,7 @@ namespace IT_Asset_Management_Project
                         break;
                     case 5:
                         break;
-                }
+                }*/
             MessageBox.Show("Exiting");
                 Application.Exit();
 
